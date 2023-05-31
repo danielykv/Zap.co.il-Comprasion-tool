2efc7c93522c0ded6db518d0c31c19d21681acf0
danielykv <ffgtf12@gmail.com>
2023-05-31 22:11:09 +0300

    Completed project, the tool is working

--- geeniemission/server.js
+++ geeniemission/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const puppeteer = require("puppeteer");
 const { google } = require("googleapis");
-
 const app = express();
 const port = 3001;
 
@@ -16,7 +15,7 @@ const auth = new google.auth.GoogleAuth({
 });
 const sheets = google.sheets({ version: "v4", auth });
 
-// Helper function to extract the spreadsheet ID from the URL
+// function to extract the spreadsheet ID from the URL
 const extractSpreadsheetId = (spreadsheetUrl) => {
   const matches = spreadsheetUrl.url.match(/\/d\/(.+)\//);
   if (matches && matches.length > 1) {
@@ -33,155 +32,177 @@ app.get("/scrape", async (req, res) => {
     }
 
     const spreadsheetId = extractSpreadsheetId(spreadsheetUrl);
-    console.log(spreadsheetId);
-    const range = "Sheet1!A:P"; // Replace with the cell range that contains the product names, links, store names, store positions, and previous store positions
-    const url = "https://www.zap.co.il/";
-    // Read product details from the Google Sheets file
+    const range = "Sheet1!A:P";
+    const url = "https://www.zap.co.il/"; //Default URL to find products
     const values = await getGoogleSheetValues(spreadsheetId, range);
-    console.log(values);
 
     // Create a browser instance
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
+    console.log("Checking for changes...");
 
-    // Iterate over the rows starting from the second row
+    // Store the rows with red flags
+    const redRows = [];
+
+    // Iterate over the rows, starting from the second row to not count the titles
     for (let i = 1; i < values.length; i++) {
-      let [productName, productLink, myStoreName, , prevStorePosition] =
-        values[i];
+      try {
+        let [productName, productLink, myStoreName, , prevStorePosition] =
+          values[i];
 
-      // Check if the product link is empty
-      if (!productLink) {
-        const newProductLink = await scrapeWebsite(browser, url, productName);
+        // Check if the product link is empty and has a valid product name
+        if (!productLink && productName) {
+          const newProductLink = await scrapeWebsite(browser, url, productName);
 
-        // Update the corresponding link in column B of the Google Sheets file
-        await updateGoogleSheet(spreadsheetId, `Sheet1!B${i + 1}`, [
-          [newProductLink],
-        ]);
+          // Update the corresponding link in column B of the Google Sheets file
+          await updateGoogleSheet(spreadsheetId, `Sheet1!B${i + 1}`, [
+            [newProductLink],
+          ]);
 
-        productLink = newProductLink;
-      }
-
-      // Check if there is a product link
-      if (productLink) {
-        let storePrices = await getStorePrices(browser, productLink);
-        let previousStorePosition;
-        // Prepare the data for updating columns E to N of the Google Sheets file
-        const updatedStoreData = [];
-
-        // Update only the first 5 stores
-        const maxStoreCount = 5;
-        for (let j = 0; j < Math.min(storePrices.length, maxStoreCount); j++) {
-          const { storeName, productPrice } = storePrices[j];
-          updatedStoreData.push(storeName, productPrice);
+          productLink = newProductLink;
         }
-        const applyConditionalFormatting = async (
-          spreadsheetId,
-          range,
-          color
-        ) => {
-          try {
-            await sheets.spreadsheets.batchUpdate({
-              spreadsheetId,
-              resource: {
-                requests: [
-                  {
-                    repeatCell: {
-                      range: {
-                        sheetId: 0, // Replace with the sheet ID if necessary
-                        ...range,
-                      },
-                      cell: {
-                        userEnteredFormat: {
-                          backgroundColor: {
-                            red: color === "green" ? 0.6 : 1,
-                            green: color === "green" ? 0.8 : 0,
-                            blue: color === "green" ? 0.6 : 0,
+
+        // Check if there is a product link
+        if (productLink) {
+          let storePrices = await getStorePrices(browser, productLink);
+          const updatedStoreData = [];
+
+          // Update only the first 5 stores (as needed, the getStorePrices func get all the stores from the product link)
+          const maxStoreCount = 5;
+          for (
+            let j = 0;
+            j < Math.min(storePrices.length, maxStoreCount);
+            j++
+          ) {
+            const { storeName, productPrice } = storePrices[j];
+            updatedStoreData.push(storeName, productPrice);
+          }
+          const applyConditionalFormatting = async (
+            spreadsheetId,
+            range,
+            color
+          ) => {
+            try {
+              await sheets.spreadsheets.batchUpdate({
+                spreadsheetId,
+                resource: {
+                  requests: [
+                    {
+                      repeatCell: {
+                        range: {
+                          sheetId: 0,
+                          ...range,
+                        },
+                        cell: {
+                          userEnteredFormat: {
+                            backgroundColor: {
+                              red: color === "green" ? 0.6 : 1,
+                              green: color === "green" ? 0.8 : 0,
+                            },
                           },
                         },
+                        fields: "userEnteredFormat.backgroundColor",
                       },
-                      fields: "userEnteredFormat.backgroundColor",
                     },
-                  },
-                ],
-              },
-            });
-            console.log("Conditional formatting applied successfully");
-          } catch (error) {
-            console.error("Error applying conditional formatting:", error);
-            throw error;
-          }
-        };
-        const matchingStore = storePrices.find(
-          (store) => store.storeName === myStoreName
-        );
-
-        if (matchingStore) {
-          const rowIndex = i + 1; // Adjust the row index accordingly
-          const columnDRange = `Sheet1!D${rowIndex}`;
-          const columnORange = `Sheet1!O${rowIndex}`;
-          const columnPRange = `Sheet1!P${rowIndex}`;
-          const updatedDataD = [[matchingStore.productPrice]];
-          const updatedDataO = [[matchingStore.storePosition]];
-
-          await updateGoogleSheet(spreadsheetId, columnDRange, updatedDataD);
-
-          // Get the existing value from Column O
-          const existingValue = await getGoogleSheetValues(
-            spreadsheetId,
-            columnORange
-          );
-          if (
-            existingValue &&
-            existingValue.length > 0 &&
-            existingValue[0] &&
-            existingValue[0].length > 0
-          ) {
-            // Update Column P with the value from Column O
-            await updateGoogleSheet(spreadsheetId, columnPRange, [
-              [existingValue[0][0]],
-            ]);
-          }
-
-          // Update the store's current position in Column O
-          await updateGoogleSheet(spreadsheetId, columnORange, updatedDataO);
-
-          const rowRange = {
-            startRowIndex: rowIndex - 1,
-            endRowIndex: rowIndex,
-            startColumnIndex: 0,
-            endColumnIndex: values[0].length,
+                  ],
+                },
+              });
+            } catch (error) {
+              console.error("Error applying conditional formatting:", error);
+              throw error;
+            }
           };
+          const matchingStore = storePrices.find(
+            (store) => store.storeName === myStoreName
+          );
 
-          // Get the previous store position from Column P
-          const previousStorePosition =
-            existingValue && existingValue.length > 0 && existingValue[0]
-              ? existingValue[0][0]
-              : null;
+          if (matchingStore) {
+            const rowIndex = i + 1;
+            const columnDRange = `Sheet1!D${rowIndex}`;
+            const columnORange = `Sheet1!O${rowIndex}`;
+            const columnPRange = `Sheet1!P${rowIndex}`;
+            const updatedDataD = [[matchingStore.productPrice]];
+            const updatedDataO = [[matchingStore.storePosition]];
 
-          if (
-            String(previousStorePosition) ===
-              String(matchingStore.storePosition) ||
-            previousStorePosition === null
-          ) {
-            // No change between previous and current positions, color the row green
-            await applyConditionalFormatting(spreadsheetId, rowRange, "green");
-          } else {
-            // Change in positions, color the row red
-            await applyConditionalFormatting(spreadsheetId, rowRange, "red");
+            await updateGoogleSheet(spreadsheetId, columnDRange, updatedDataD);
+
+            // Get the existing value from Column O
+            const existingValue = await getGoogleSheetValues(
+              spreadsheetId,
+              columnORange
+            );
+            if (
+              existingValue &&
+              existingValue.length > 0 &&
+              existingValue[0] &&
+              existingValue[0].length > 0
+            ) {
+              // Update Column P with the value from Column O
+              await updateGoogleSheet(spreadsheetId, columnPRange, [
+                [existingValue[0][0]],
+              ]);
+            }
+
+            // Update the store's current position in Column O
+            await updateGoogleSheet(spreadsheetId, columnORange, updatedDataO);
+
+            const rowRange = {
+              startRowIndex: rowIndex - 1,
+              endRowIndex: rowIndex,
+              startColumnIndex: 0,
+              endColumnIndex: values[0].length,
+            };
+
+            // Get the previous store position from Column P
+            const previousStorePosition =
+              existingValue && existingValue.length > 0 && existingValue[0]
+                ? existingValue[0][0]
+                : null;
+
+            if (
+              String(previousStorePosition) ===
+                String(matchingStore.storePosition) ||
+              previousStorePosition === null
+            ) {
+              // No change between previous and current positions, color the row green
+              await applyConditionalFormatting(
+                spreadsheetId,
+                rowRange,
+                "green"
+              );
+            } else {
+              // Change in positions, color the row red
+              await applyConditionalFormatting(spreadsheetId, rowRange, "red");
+
+              // Store the row with red color
+              redRows.push({
+                storeName: matchingStore.storeName,
+                productName: productName,
+                productPrice: matchingStore.productPrice,
+                url: productLink,
+                currentPosition: matchingStore.storePosition,
+                previousPosition: previousStorePosition,
+              });
+            }
           }
-        }
 
-        // Update columns E to N with store names and prices
-        await updateGoogleSheet(spreadsheetId, `Sheet1!E${i + 1}:N${i + 1}`, [
-          updatedStoreData,
-        ]);
+          // Update columns E to N with store names and prices
+          await updateGoogleSheet(spreadsheetId, `Sheet1!E${i + 1}:N${i + 1}`, [
+            updatedStoreData,
+          ]);
+        }
+      } catch (error) {
+        console.error("Error processing row:", error);
+        redRows.push({
+          error: `Error processing row ${i}`,
+        });
       }
     }
 
-    // Close the browser instance
     await browser.close();
 
     console.log("Web scraping and updating completed successfully");
-    res.send("Web scraping and updating completed successfully");
+
+    res.json(redRows);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Error occurred during web scraping and updating.");
@@ -189,9 +210,11 @@ app.get("/scrape", async (req, res) => {
 });
 
 const getStorePrices = async (browser, productLink) => {
-  const page = await browser.newPage({ headless: false });
-
-  await page.goto(productLink);
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
+  );
+  await page.goto(productLink, { waitUntil: "domcontentloaded" });
 
   const divElements = await page.$$(`.compare-item-row`);
 
@@ -206,21 +229,22 @@ const getStorePrices = async (browser, productLink) => {
 
     // Check if the product store name is available
     if (productStoreName) {
-      const productPrice = await page.evaluate(
+      let productPrice = await page.evaluate(
         (element) => element.getAttribute("data-total-price"),
         divElement
       );
-      const storePosition = i + 1; // Position based on the store's index in the array/list
+
+      // Convert the product price to an integer without decimal places, because the browser get price with extra zeros, like XXX.0000
+      productPrice = Math.floor(parseFloat(productPrice));
 
       storePrices.push({
         storeName: productStoreName,
         productPrice,
-        storePosition: i + 1, // Position based on the store's index in the array/list
+        storePosition: i + 1, // Position based on the store's index
       });
     }
   }
 
-  console.log(storePrices);
   return storePrices;
 };
 
@@ -239,11 +263,14 @@ const getGoogleSheetValues = async (spreadsheetId, range) => {
   }
 };
 
+//Function that searches the product and gets its link
 const scrapeWebsite = async (browser, url, productName) => {
-  const page = await browser.newPage({ headless: false });
-
-  await page.goto(url);
-
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
+  );
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  await page.waitForSelector(".selection");
   await page.click(".selection");
   await page.type(".select2-search__field", productName);
   await page.waitForSelector(".select2-result-complexText");
@@ -256,7 +283,7 @@ const scrapeWebsite = async (browser, url, productName) => {
 
 const updateGoogleSheet = async (spreadsheetId, range, values) => {
   try {
-    await sheets.spreadsheets.values.update({
+    sheets.spreadsheets.values.update({
       spreadsheetId,
       range,
       valueInputOption: "RAW",
@@ -264,7 +291,6 @@ const updateGoogleSheet = async (spreadsheetId, range, values) => {
         values,
       },
     });
-    console.log("Google Sheets updated successfully");
   } catch (error) {
     console.error("Error updating Google Sheets:", error);
     throw error;
--- geeniemission/src/App.js
+++ geeniemission/src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [result, setResult] = useState("");
+  const [redRows, setRedRows] = useState([]); // State to get red rows
+  const [showAnimation, setShowAnimation] = useState(false); // State to control animation
+  const [resultMessage, setResultMessage] = useState(""); // State for result message
+  const [loading, setLoading] = useState(false); // State for loading status
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -11,30 +14,108 @@ function App() {
 
   const handleSubmit = async () => {
     try {
+      setLoading(true);
+      setResultMessage("Loading...");
+
       const response = await fetch(
         `http://localhost:3001/scrape?url=${inputValue}`
       );
-      const data = await response.text();
-      setResult(data); // Update the component state with the scrape result
+      const data = await response.json();
+      setRedRows(data);
+
+      if (data.length > 0) {
+        setResultMessage(`We found ${data.length} changes in your file:`);
+      } else {
+        setResultMessage(
+          "Scraping completed successfully! there are no any changes"
+        );
+      }
+      if (!response.ok) {
+        const errorData = await response.text();
+        setResultMessage(`Error occurred during scraping: ${errorData}`);
+      }
     } catch (error) {
       console.error("Error:", error);
+      setResultMessage("Error occurred during scraping.");
+    } finally {
+      setLoading(false);
     }
   };
 
+  const renderTable = () => {
+    if (redRows.length === 0) {
+      return null;
+    }
+
+    return (
+      <table className={`table ${showAnimation ? "fadeInAnimation" : ""}`}>
+        <thead>
+          <tr>
+            <th>Store Name</th>
+            <th>Product Name</th>
+            <th>Product Price</th>
+            <th>Product Link</th>
+            <th>Current Position</th>
+            <th>Previous Position</th>
+          </tr>
+        </thead>
+        <tbody>
+          {redRows.map((row, index) => (
+            <tr
+              key={index}
+              className={`${showAnimation ? "gradientAnimation" : ""}`}
+            >
+              <td>{row.storeName}</td>
+              <td>{row.productName}</td>
+              <td>{row.productPrice + "₪"}</td>
+              <td>
+                <a
+                  href={row.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="button-link"
+                >
+                  Visit Website
+                </a>
+              </td>
+              <td>{row.currentPosition}</td>
+              <td>{row.previousPosition}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  useEffect(() => {
+    // Enable animation after the component mounts
+    setShowAnimation(true);
+  }, []);
+
   return (
-    <div className="background">
-      <div className="container">
-        <h1>React Input Demo</h1>
-        <div className="input-container">
+    <div className={`background ${showAnimation ? "gradientAnimation" : ""}`}>
+      <div className={`container ${showAnimation ? "fadeInAnimation" : ""}`}>
+        <img src="geeniexzaplogo.png" alt="logo" />
+        <h1>Welcome to our comparison tool!</h1>
+        <p>Please enter your Google Spreadsheet link</p>
+        <div for="search" className="input-container">
           <input
             type="text"
             value={inputValue}
             onChange={handleInputChange}
-            placeholder="Enter the website URL"
+            placeholder="Enter the spreadsheet URL"
           />
-          <button onClick={handleSubmit}>Scrape Website</button>
+          <button type="submit" onClick={handleSubmit}>
+            Scrape Website
+          </button>
         </div>
-        {result && <div className="result">{result}</div>}
+        {loading ? (
+          <p className="result">Loading...</p>
+        ) : (
+          resultMessage && <p className="result">{resultMessage}</p>
+        )}{" "}
+        <h2 className="credit">Made by Daniel Yakubov</h2>
+        {renderTable()}
       </div>
     </div>
   );
